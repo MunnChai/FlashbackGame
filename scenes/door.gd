3acbc79d91eb50d758870851d828047b68300743
jaqlynai <119989117+jaqlynai@users.noreply.github.com>
2024-02-21 00:02:42 -0800

    started door !

---
+++ scenes/door.gd
@@ -0,0 +1,12 @@
+class_name Door
+extends Area2D
+
+var animator
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animator = $AnimatedSprite2D # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
