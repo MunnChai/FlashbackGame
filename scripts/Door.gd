0d19bc37d4baf87819eaa6764e71c58a974e7b7f
jaqlynai <119989117+jaqlynai@users.noreply.github.com>
2024-02-21 01:16:02 -0800

    FINISHED DOOR

---
+++ scripts/Door.gd
@@ -0,0 +1,36 @@
+class_name Door
+extends Area2D
+
+var player
+@export var destination: Vector2 = Vector2(0, 0)
+
+var animator
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animator = $AnimatedSprite2D # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func interact():
+	if (!player):
+		return
+	teleport()
+
+func _on_body_entered(body):
+	if (body is Player):
+		player = body
+		player.add_interactible(self)
+		print("entered door")
+		
+func _on_body_exited(body):
+	if (body is Player):
+		player.remove_interactible(self)
+		player = null
+		print("exited door")
+
+# requires player!
+func teleport():
+	player.position = destination
