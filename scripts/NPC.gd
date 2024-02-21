6c3cf489aba242e5cecf56aae30c7c818af0f56e
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2024-02-21 00:02:11 -0800

    Reworked interactibles

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -4,9 +4,10 @@ extends CharacterBody2D
 const WALK_SPEED = 75
 const RUN_SPEED = 150
 
-signal signal_interact(interactible)
+@export var npc_name: String
+@export var conversations: PackedStringArray
 
-var player_in
+var player
 
 var animator
 
@@ -14,25 +15,26 @@ func _physics_process(delta):
 	
 	handle_movement()
 
-
 func handle_movement():
 	move_and_slide()
-
 
 func _ready():
 	animator = $AnimatedSprite2D
 
 func interact():
-	if (!player_in):
+	if (!player):
 		return
-	signal_interact.emit(self)
+	var conv = conversations[randi_range(0, 1)]
+	player.start_dialogue(npc_name, conv)
 
 func _on_body_entered(body):
 	if (body is Player):
-		player_in = true
+		player = body
+		player.add_interactible(self)
 		animator.animation = "player_in"
 
 func _on_body_exited(body):
 	if (body is Player):
-		player_in = false
+		player.remove_interactible(self)
+		player = null
 		animator.animation = "player_out"
