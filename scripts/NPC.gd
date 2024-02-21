1d8781b90a0630617fb181f7fb08c32af11df995
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2024-02-20 22:21:29 -0800

    Added "dialogue" and npc behavioru

--- scripts/NPC.gd
+++ scripts/NPC.gd
@@ -4,7 +4,11 @@ extends CharacterBody2D
 const WALK_SPEED = 75
 const RUN_SPEED = 150
 
+signal signal_interact(interactible)
 
+var player_in
+
+var animator
 
 func _physics_process(delta):
 	
@@ -13,3 +17,22 @@ func _physics_process(delta):
 
 func handle_movement():
 	move_and_slide()
+
+
+func _ready():
+	animator = $AnimatedSprite2D
+
+func interact():
+	if (!player_in):
+		return
+	signal_interact.emit(self)
+
+func _on_body_entered(body):
+	if (body is Player):
+		player_in = true
+		animator.animation = "player_in"
+
+func _on_body_exited(body):
+	if (body is Player):
+		player_in = false
+		animator.animation = "player_out"
